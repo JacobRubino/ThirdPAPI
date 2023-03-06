f0e4233d264f6047844ab92100ff4249ad7d493b
Jacob Rubino <jacobrubino@gmail.com>
2023-03-06 13:53:22 -0500

    added local storage saving and code persisting on refresh/ revisit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let timeSec = document.querySelectorAll(`[class*="time-block"]`);
 let headTimer = document.querySelector('#currentDay')
+let saveButt = document.getElementsByClassName('saveBtn')
 
 let block9 = $('#hour-9')
 let block10 = $('#hour-10')
@@ -23,14 +24,15 @@ let scheduleArr = [
   block5,
 ]
 
+// loop through the times and set the class to set the color, checking if i can set localstorage while looping through if i saved the content as the div id
 function setCalColor (){
   for (let index = 0; index < scheduleArr.length; index++) {
-    // let childText = scheduleArr[index].children().first().text()
-    // console.log(childText)
     var x = scheduleArr[index].get(0)
-    let time_now = dayjs().format('h')
+    let time_now = dayjs().format('H')
     blockCol = scheduleArr[index].data('hour')
-    console.log(x)
+    // console.log(x)
+    // console.log(time_now)
+    // console.log(blockCol)
     // if i set them all as future by default would i only have to loop until i reach the present?
     if (blockCol < Number(time_now)){ //in the past
         x.classList.add("past")      
@@ -41,17 +43,52 @@ function setCalColor (){
     }
   }
 }
+
+function setTextCont(){
+  for (let z = 0; z < scheduleArr.length; z++) {
+    // console.log(z)
+    t = `scheduleArr[${z}]`
+    StoredCont = localStorage.getItem(t)
+    console.log(StoredCont)
+    a = scheduleArr[z][0].children[1]
+    a.value = StoredCont
+  }
+  // for (let index = 0; index < scheduleArr.length; index++)
+  //   z = index
+    // t = `scheduleArr[${z-8}]`
+    // var StoredCont = localStorage.getItem(t)
+    // a = scheduleArr[i][0].children[1]
+    // a.value(StoredCont)
+    
+    // console.log(a)
+
+}
+
+// just get the time and format it then put on top of the page
 function current_time(){
   let today = dayjs();
   $('#currentDay').text(today.format('MMM D, YYYY')); 
 }
 
+function saveClick () {
+  var parent = $(this).closest('.time-block').data('hour')
+  var storeThis = $(this).closest('.time-block')[0].children[1].value
+  localStorage.setItem(`scheduleArr[${parent - 9}]`, storeThis)
+}
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+for (let index = 0; index < saveButt.length; index++) { 
+  const element = saveButt[index];
+  element.addEventListener("click", saveClick) 
+}
 
 $(function () {
-
+  current_time()
+  setCalColor()
+  setTextCont()
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -71,5 +108,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
-current_time()
-setCalColor()
+
+
